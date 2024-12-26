adf6505a6ca86675c52aafed2a5f701fab78ea41
pablozr <apollo.zero@outlook.com>
2024-12-26 11:59:11 -0300

    atualizaaaaaa

--- src/MatrizEsparsaEstatica.java
+++ src/MatrizEsparsaEstatica.java
@@ -29,4 +29,54 @@ public class MatrizEsparsaEstatica {
         }
         return true;
     }
+
+    public boolean isLinha() {
+        boolean encontrouLinha = false;
+        for (int i = 0; i < linhas; i++) {
+            boolean linhaNaoVazia = false;
+            for (int j = 0; j < colunas; j++) {
+                if (matriz[i][j] != 0) {
+                    linhaNaoVazia = true;
+                }
+            }
+            if (linhaNaoVazia) {
+                if (encontrouLinha) {
+                    return false;
+                }
+                encontrouLinha = true;
+            }
+        }
+        return encontrouLinha;
+    }
+
+    public boolean isColuna() {
+        boolean encontrouColuna = false;
+        for (int j = 0; j < colunas; j++) {
+            boolean colunaNaoVazia = false;
+            for (int i = 0; i < linhas; i++) {
+                if (matriz[i][j] != 0) {
+                    colunaNaoVazia = true;
+                }
+            }
+            if (colunaNaoVazia) {
+                if (encontrouColuna) {
+                    return false;
+                }
+                encontrouColuna = true;
+            }
+        }
+        return encontrouColuna;
+    }
+
+    public boolean isTriangularInferior() {
+        for (int i = 0; i < linhas; i++) {
+            for (int j = i + 1; j < colunas; j++) {
+                if (matriz[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
