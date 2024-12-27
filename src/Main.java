2dfea2ea42f1ce1bce515990ae9c475ce8fef49a
Matheus1421 <matheusdafonsecamarques@gmail.com>
2024-12-27 14:11:29 -0300

    Teste dos métodos 1 - 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        MatrizEsparsaEstatica matriz = new MatrizEsparsaEstatica(3, 3);
+
+        // Inserir elementos
+        matriz.insertElement(0,0, 1);
+        matriz.insertElement(0,1, 2);
+        matriz.insertElement(0,2, 3);
+        matriz.insertElement(1,0, 4);
+        matriz.insertElement(1,1, 5);
+        matriz.insertElement(1,2, 6);
+        matriz.insertElement(2,0, 7);
+        matriz.insertElement(2,1, 8);
+        matriz.insertElement(2,2, 9);
+
+        // Imprimir matriz
+        matriz.printMatriz();
+
+        System.out.println("--------------------------------------------------------------------");
+
+        //Remover um elemento
+        matriz.removeElement(1,2);
+
+        //Impressão de matriz após remoção do 6
+        matriz.printMatriz();
+
+        System.out.println("--------------------------------------------------------------------");
+
+        //Busca por um elemento
+        matriz.searchElement(4);
+
+        //Imprimir uma matriz vazia
+        matriz.printMatrizVazia();
     }
 }
--- src/MatrizEsparsaEstatica.java
+++ src/MatrizEsparsaEstatica.java
@@ -1,13 +1,58 @@
 public class MatrizEsparsaEstatica {
-    private final int linhas;
-    private final int colunas;
-    private final int[][] matriz;
+    private int linhas;
+    private int colunas;
+    private int[][] matriz;
 
     public MatrizEsparsaEstatica(int linhas, int colunas) {
         this.linhas = linhas;
         this.colunas = colunas;
         this.matriz = new int[linhas][colunas];
     }
+
+    //1 - Inserir um elemento
+    public void insertElement(int linha, int coluna, int valor) {
+        matriz[linha][coluna] = valor;
+    }
+
+    //2 - Remover um elemento
+    public void removeElement(int linha, int coluna) {
+        matriz[linha][coluna] = 0;
+    }
+
+    //3 - Busca por um elemento específico (pode ser feito como booleano e retornando true/false)
+    public boolean searchElement(int elem) {
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                if (matriz[i][j] == elem){
+                    System.out.println("Elemento " + elem + " encontrado, na posiçã  o [" + i + ", " + j + "]");
+                    return true;
+                }
+            }
+        }
+
+        System.out.println("Elemento não encontrado");
+        return false;
+    }
+
+    //4 - Impressão da matriz
+    public void printMatriz() {
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.printf("[%d][%d]: [%d] \n", i, j, matriz[i][j]);
+            }
+        }
+    }
+
+    //5 - Representar uma matriz vazia
+    public void printMatrizVazia (){
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.printf("[%d][%d]: 0 \n", i, j);
+            }
+        }
+    }
+
+    //6 - Verificar se é uma matriz vazia
     public boolean isVazia() {
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
@@ -19,6 +64,7 @@ public class MatrizEsparsaEstatica {
         return true;
     }
 
+    //7 - Verificar se é uma matriz diagonal (só tem elementos na diagonal principal)
     public boolean isDiagonal() {
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
@@ -30,6 +76,7 @@ public class MatrizEsparsaEstatica {
         return true;
     }
 
+    //8 - Verificar se é uma matriz linha (só tem elementos em uma única linha)
     public boolean isLinha() {
         boolean encontrouLinha = false;
         for (int i = 0; i < linhas; i++) {
@@ -49,6 +96,7 @@ public class MatrizEsparsaEstatica {
         return encontrouLinha;
     }
 
+    //9 - Verificar se é uma matriz coluna (só tem elementos em uma única coluna)
     public boolean isColuna() {
         boolean encontrouColuna = false;
         for (int j = 0; j < colunas; j++) {
@@ -68,6 +116,8 @@ public class MatrizEsparsaEstatica {
         return encontrouColuna;
     }
 
+    //10 - Verificar se é uma matriz triangular inferior (só tem elementos da diagonal
+    //principal para baixo)
     public boolean isTriangularInferior() {
         for (int i = 0; i < linhas; i++) {
             for (int j = i + 1; j < colunas; j++) {
