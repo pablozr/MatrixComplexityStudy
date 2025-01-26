b7fc3231946e6f7b858d5c82465f5bf6ad78e180
pablozr <apollo.zero@outlook.com>
2025-01-26 11:45:00 -0300

    Atualização final

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,12 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-        //TESTES ESPARSA NORMAL
 
-        /*MatrizEsparsaEstatica matriz = new MatrizEsparsaEstatica(3, 3);
-=======
         int[] tamanhos = {10, 20, 30, 40, 50, 100, 200, 500, 1000, 10000};
->>>>>>> Stashed changes
 
         try (FileWriter writer = new FileWriter("desempenho_matrizes.csv", StandardCharsets.UTF_8)) {
             writer.write("Tamanho,Metodo,Classe,TempoMedio\n");
 
-<<<<<<< Updated upstream
-        //transposta
-        //matriz.obterTransposta().printMatriz();
-=======
             for (int tamanho : tamanhos) {
                 System.out.println("Testando matrizes de tamanho: " + tamanho);
 
@@ -50,38 +41,9 @@ public class Main {
                     long tempoMedio = tempoTotal / 10;
                     double tempoEmSegundos = tempoMedio / 1_000_000_000.0;
                     String tempoFormatado = String.format("%.6f", tempoEmSegundos);
->>>>>>> Stashed changes
 
                     writer.write(tamanho + ";" + metodo + ",MatrizEsparsaEstatica;" + tempoFormatado + "\n");
 
-<<<<<<< Updated upstream
-        //triangular superior
-        matriz.isTriangularSuperior();*/
-
-        //TESTES ESPARSA LISTA ENCADEADA
-
-        MatrizEsparsaLinkedList matriz = new MatrizEsparsaLinkedList(3);
-
-        matriz.insertElement(0,0, 1);
-        matriz.insertElement(0,1, 2);
-        matriz.insertElement(0,2, 3);
-        matriz.insertElement(1,0, 4);
-        matriz.insertElement(1,1, 5);
-        matriz.insertElement(1,2, 6);
-        matriz.insertElement(2,0, 7);
-        matriz.insertElement(2,1, 8);
-        matriz.insertElement(2,2, 9);
-
-        matriz.printMatriz();
-
-        matriz.removeElement(1,1);
-
-        System.out.println("-----------------------------------------------");
-
-        matriz.printMatriz();
-
-        System.out.println(matriz.searchElement(2, 2));
-=======
                     long tempoTotalEncadeada = 0;
                     for (int i = 0; i < 10; i++) {
                         long startTime = System.nanoTime();
@@ -107,11 +69,8 @@ public class Main {
             }
 
 
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
->>>>>>> Stashed changes
     }
 }
